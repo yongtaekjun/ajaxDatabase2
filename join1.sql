bd52b635e24ebd6b88c7c074568e370373288926
yongtaekjun <32220288+yongtaekjun@users.noreply.github.com>
2017-11-14 13:47:14 -0500

    deleted not needed files ..

--- join1.sql
+++
@@ -1,4 +0,0 @@
-select countryName from country INNER JOIN
-hotel on country.countryID = hotel.countryID INNER JOIN
-booking on booking.hotelID = hotel.hotelID INNER JOIN
-traveler on traveler.travelerID = booking.travelerID and traveler.travelerLastName = 'Clooney';
--- join2.sql
+++
@@ -1,5 +0,0 @@
-select countryName from country,hotel,booking,traveler where 
-country.countryID = hotel.countryID and 
-booking.hotelID = hotel.hotelID and
-traveler.travelerID = booking.travelerID and
-traveler.travelerLastName = 'Clooney';
